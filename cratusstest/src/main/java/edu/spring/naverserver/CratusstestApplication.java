726fcf9df02ad05d2402e835c3616a1d335a1cbd
cratuss <ldoh92@gmail.com>
2021-10-16 11:31:59 +0900

    cratuss

---
+++ cratusstest/src/main/java/edu/spring/naverserver/CratusstestApplication.java
@@ -0,0 +1,14 @@
+package edu.spring.naverserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CratusstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CratusstestApplication.class, args);
+		System.out.println("cratuss");
+	}
+
+}
